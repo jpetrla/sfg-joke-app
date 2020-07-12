bb18809b7ad8078aa236271f3a912bb3c7075a97
Jan Petrla <j.petrla@gmail.com>
2020-07-12 23:44:29 +0200

    Initial project add

---
+++ src/main/java/cz/jpetrla/sfgjokeapp/SfgJokeAppApplication.java
@@ -0,0 +1,13 @@
+package cz.jpetrla.sfgjokeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgJokeAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SfgJokeAppApplication.class, args);
+    }
+
+}
