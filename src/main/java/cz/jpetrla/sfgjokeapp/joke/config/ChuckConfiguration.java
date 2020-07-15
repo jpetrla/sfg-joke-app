22ca168f4f97ffaaa6430d116faa948c617475a3
Jan Petrla <j.petrla@gmail.com>
2020-07-15 22:57:59 +0200

    java based configuration

---
+++ src/main/java/cz/jpetrla/sfgjokeapp/joke/config/ChuckConfiguration.java
@@ -0,0 +1,14 @@
+package cz.jpetrla.sfgjokeapp.joke.config;
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ChuckConfiguration {
+
+    @Bean
+    public ChuckNorrisQuotes chuckNorrisQuotes() {
+        return new ChuckNorrisQuotes();
+    }
+}
